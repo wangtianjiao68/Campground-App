a0db663ba9293757a86127fe42c37be5b23a132d
tianjiao68 <wangtianjiao0608@hotmail.com>
2020-07-29 23:26:55 +0000

    start

--- app.js
+++ app.js
@@ -30,8 +30,6 @@ var seedDB = require("./seeds.js");// seedDB prefilled data
 //set mongoose
 const mongoose = require('mongoose');//for data
 
-//mongodb+srv://Tianjiao:<password>@tianjiaocluster.g6vnz.mongodb.net/<dbname>?retryWrites=true&w=majority
- //mongoose.connect('mongodb+srv://Tianjiao:682415@tianjiaocluster.g6vnz.mongodb.net/yelp_camp?retryWrites=true&w=majority', {
 //mongoose.connect('mongodb://localhost:27017/yelp_camp', {
 var url = process.env.DATABASEURL;
 mongoose.connect(url, {
